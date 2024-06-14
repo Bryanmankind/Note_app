6e8c0721df97b77bf802fe76c7fd58be94840a85
Bryanmankind <shegeemankind@gmail.com>
2024-06-14 01:52:35 +0100

    React Query

---
+++ note-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="body" >
+      <div className="container">
+      <h2>Title</h2>
+      <textarea rows="5"></textarea>
+      <button>Add note</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
